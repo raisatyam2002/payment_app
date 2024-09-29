8c203ab1fc7f31219b35ca755fb1aadc1dfc4cb3
satyam rai <raisatyam121@gmail.com>
2024-09-29 19:46:16 +0530

    fixed alert

--- packages/ui/src/AppBar.tsx
+++ packages/ui/src/AppBar.tsx
@@ -17,7 +17,6 @@ export const AppBar = ({ signIn, signOut, user, callbackUrl }: AppBarProps) => {
   }
   async function handleSignOut() {
     await signOut();
-    alert(callbackUrl);
     window.location.href = callbackUrl;
   }
   return (
