05af5744cbdcb2461a63e4e4059b4e07ddfe12da
satyam rai <raisatyam121@gmail.com>
2024-10-05 01:36:53 +0530

    fixed minor bugs

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -8,7 +8,7 @@ export function Card({
   children?: React.ReactNode;
 }): JSX.Element {
   return (
-    <div className="border p-4 relative" style={{ zIndex: "-1" }}>
+    <div className="border p-4 relative">
       <h1
         className="text-xl border-b  "
         style={{
